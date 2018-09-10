f47b5b1108c8f8cf27ffec6996adaf415c9c5bc6
yuuukihara <yuukihara0219@gmail.com>
2018-09-10 12:53:19 +0900

    test

---
+++ 0907/sql/ecsite.sql
@@ -0,0 +1,32 @@
+set names utf8;
+set foreign_key_checks = 0;
+drop database if exists ecsite;
+
+create database if not exists ecsite;
+use ecsite;
+
+drop table if exists login_user_transaction;
+
+create table login_user_transaction(
+id int not null primary key auto_increment,
+login_id varchar(16) unique,
+login_pass varchar(16),
+user_name varchar(50),
+insert_date datetime,
+updated_date datetime
+);
+
+drop table if exists user_buy_item_transaction;
+
+createa table user_buy_transaction(
+id int not null primary key auto_increment,
+item_transaction_id int,
+total_price int,
+total_count int,
+user_master_id varchar(16),
+pay varchar(30),
+insert_date datetime,
+delete_date datetime
+);
+
+INSERT INTO item_info_transaction
